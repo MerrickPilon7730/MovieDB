e30daeebbf049f6fdadbe8b6ed47537b00bb93b0
MerrickPilon7730 <mpilon7730@conestogac.on.ca>
2025-03-16 13:56:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/prog3210_assignment2/view/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.prog3210_assignment2.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.prog3210_assignment2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
