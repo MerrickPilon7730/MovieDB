7aba4c8efa16e79f6e2234d57ffed4dfe6e2b647
MerrickPilon7730 <mpilon7730@conestogac.on.ca>
2025-03-16 15:46:17 -0400

    Update MovieViewModel.kt

--- app/src/main/java/com/example/prog3210_assignment2/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/prog3210_assignment2/viewmodel/MovieViewModel.kt
@@ -1,14 +1,24 @@
 package com.example.prog3210_assignment2.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.prog3210_assignment2.model.MovieModel
 
 class MovieViewModel : ViewModel(){
 
-    private val movieData = MutableLiveData<MovieModel>();
-    val movieModel = MovieModel();
+    private val _movieData = MutableLiveData<MovieModel>()
+    val movieData: LiveData<MovieModel> get() = _movieData
 
+    val movieModel = MovieModel()
+
+    fun setMovieData(movie: MovieModel){
+        _movieData.value = movie
+    }
+
+    fun searchForMovie(movieTitle: String) {
+        val urlString = "http://www.omdbapi.com/?apikey=7c3e78d1&s=$movieTitle&type=movie"
+    }
 }
 
 
