1dd07f1fbbc420b46e5314a19c1f32af4f5fcc7f
Rikhi Singh <114336052+RikhiSingh@users.noreply.github.com>
2025-04-07 22:31:56 -0400

    refact: removed unused deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,6 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
-    implementation(libs.firebase.analytics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
